27eda00a95ff45b94ec90f401a2a09e366f83e48
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-08-26 17:16:49 -0500

    installers

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="WoRMStools",
+      version='1.0',
+      author='Ulises Rosas',
+      author_email='ulisesfrosasp@gmail.com',
+      url='https://github.com/Ulises-Rosas/OBISdat',
+      packages = ['WoRMStools'],
+      package_dir = {'WoRMStools': 'src'},
+      scripts = ['src/worms.py']
+      )
+
