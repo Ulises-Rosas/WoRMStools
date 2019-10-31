dceeb61dcb0bf00ffbec4e7a055dfc3314ca8196
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-10-31 15:38:36 -0500

    let to use readme file at PyPi

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@
 import setuptools
 from distutils.core import setup
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setup(name="WoRMStools",
-      version='1.0',
+      version='1.3',
+      long_description = readme,
+      long_description_content_type='text/markdown',
       author='Ulises Rosas',
       author_email='ulisesfrosasp@gmail.com',
-      url='https://github.com/Ulises-Rosas/OBISdat',
+      url='https://github.com/Ulises-Rosas/WoRMStools',
       packages = ['WoRMStools'],
       package_dir = {'WoRMStools': 'src'},
       scripts = ['src/worms.py'],
-      setup_requires=['wheel']
+      setup_requires=['wheel'],
+      classifiers=['Programming Language :: Python :: 3']
       )
 
