faab7e326be83d6a90533cdcdded4af8f6cabab8
Ulises-Rosas <ulisesfrosasp@gmail.com>
2019-11-01 22:02:35 -0500

    Sub versio test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(name="WoRMStools",
-      version='1.4',
+      version='1.3.1',
       long_description = readme,
       long_description_content_type='text/markdown',
       author='Ulises Rosas',
